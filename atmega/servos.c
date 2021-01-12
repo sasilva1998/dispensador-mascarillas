184d239d15a537e687c36cecb69dc0b13bfd6ade
sasilva1998 <sasilva1998@gmail.com>
2021-01-11 19:04:05 -0500

    servo functions

---
+++ atmega/servos.c
@@ -0,0 +1,58 @@
+#include "servos.h"
+
+void
+initServos ()
+{
+  // se usa 16 mhz
+
+  DDRB |= 1 << PINB1;		// Set pin 9 on arduino to output
+  DDRB |= 1 << PINB2;
+  DDRB |= 1 << PINB3;
+  /* 1. Set Fast PWM mode 14: set WGM11, WGM12, WGM13 to 1 */
+  /* 3. Set pre-scaler of 8 */
+  /* 4. Set Fast PWM non-inverting mode */
+  TCCR1A |= (1 << WGM11) | (1 << COM1A1) | (1 << COM1B1);
+  TCCR1B |= (1 << WGM12) | (1 << WGM13) | (1 << CS11);
+  /* 2. Set ICR1 register: PWM period */
+  ICR1 = 39999;
+
+}
+
+void
+posicionServos (int id, int pos)
+{
+
+  if (id == 1)
+    {
+      if (pos >= 0)
+	{
+	  pos = (pos + 2) * 5.55;
+	  pos = pos + 2989;
+	  OCR1A = pos;
+	}
+      if (pos < 0)
+	{
+	  pos = (abs (pos)) * 5.55;
+	  pos = 2999 - pos;
+	  OCR1A = pos;
+	}
+
+    }
+
+  if (id == 2)
+    {
+      if (pos >= 0)
+	{
+	  pos = (pos + 2) * 5.55;
+	  pos = pos + 2989;
+	  OCR1B = pos;
+	}
+      if (pos < 0)
+	{
+	  pos = (abs (pos)) * 5.55;
+	  pos = 2999 - pos;
+	  OCR1B = pos;
+	}
+
+    }
+}
---
+++ atmega/servos.h
@@ -0,0 +1,13 @@
+#ifndef __AVR_ATmega328P__
+#define __AVR_ATmega328P__
+#endif
+
+#define F_CPU          16000000UL
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+
+void initServos ();
+
+void posicionServos ();
